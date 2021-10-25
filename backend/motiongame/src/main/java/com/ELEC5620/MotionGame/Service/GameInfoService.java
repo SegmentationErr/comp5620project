e8869fdc3338333529ab182a45bbe366b5ff8a16
devtsai <devtsai@outlook.com>
2021-10-25 20:08:13 +1100

    Game Creation API backend finished.

---
+++ backend/motiongame/src/main/java/com/ELEC5620/MotionGame/Service/GameInfoService.java
@@ -0,0 +1,23 @@
+package com.ELEC5620.MotionGame.Service;
+
+import com.ELEC5620.MotionGame.Mapper.GameInfoMapper;
+import com.ELEC5620.MotionGame.Mapper.GameScoreMapper;
+import com.ELEC5620.MotionGame.Model.GameInfoModel;
+import com.ELEC5620.MotionGame.Model.GameScoreModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GameInfoService {
+    private final GameInfoMapper dao;
+
+    @Autowired
+    public GameInfoService(GameInfoMapper dao) {
+        this.dao = dao;
+    }
+
+
+    public boolean insert(GameInfoModel model) {
+        return dao.insert(model) > 0;
+    }
+}
