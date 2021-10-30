69cb312b49b061d1601d7896006073181cb8733a
devtsai <devtsai@outlook.com>
2021-10-30 17:18:05 +1100

    Try to add cors 1

---
+++ backend/motiongame/src/main/java/com/ELEC5620/MotionGame/Config/CrossOriginConfig.java
@@ -0,0 +1,17 @@
+package com.ELEC5620.MotionGame.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class CrossOriginConfig implements WebMvcConfigurer {
+    static final String ORIGINS[] = new String[]{"GET", "POST", "PUT", "DELETE"};
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping("/**").allowedOrigins("*").allowCredentials(true).allowedMethods(ORIGINS).maxAge(3600);
+    }
+}
