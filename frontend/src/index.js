c0ef5823e48bbdc38595e4f5fdf2a64eae8d0bb0
charles <yjc1320802502@gmail.com>
2021-10-30 16:11:37 +0800

    fix COR and start gamepage

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './components/HomePage';
+import PlayGamePage from './components/PlayGamePage';
 
 ReactDOM.render(
   <Router>
     <div>
       <Switch>
         <Route path="/" component={HomePage} />
+        {/* <Route path="/" component={PlayGamePage} /> */}
 
       </Switch>
     </div>
